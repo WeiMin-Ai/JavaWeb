10024388d7b93f224648ae31e6af36bd944f3da7
WeiMin-Coding <badmanweimin@gmail.com>
2024-09-04 23:24:31 +0800

    Map、List转JSON

--- JsonDemo/src/com/java/test/TestJson.java
+++ JsonDemo/src/com/java/test/TestJson.java
@@ -6,6 +6,9 @@ import com.java.pojo.Dog;
 import com.java.pojo.Person;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class TestJson {
     @Test
     public void testWriteJson() throws Exception{
@@ -23,5 +26,25 @@ public class TestJson {
         String personString = "{\"age\":555,\"dog\":{\"name\":\"小黄\"},\"name\":\"WayneMing\"}\n";
         Person wayneMing = JSON.parseObject(personString, Person.class);
         System.out.println(wayneMing);
+    }
+
+    @Test
+    public void testMapJson() throws Exception{
+        HashMap<String, String> hashMap = new HashMap<String, String>();
+        hashMap.put("age", "555");
+        hashMap.put("name", "WayneMing");
+        String jsonString = JSON.toJSONString(hashMap);
+        System.out.println(jsonString);
+    }
+
+    @Test
+    public void testListJson() throws Exception{
+        Dog dog = new Dog("小黄");
+        Person wayneMing = new Person("WayneMing", 555, dog);
+        ArrayList<Person> arrayList = new ArrayList<>();
+        arrayList.add(wayneMing);
+
+        String jsonString = JSON.toJSONString(arrayList);
+        System.out.println(jsonString);
     }
 }
