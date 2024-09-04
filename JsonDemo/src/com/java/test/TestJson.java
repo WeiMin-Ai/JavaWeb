ed960f676a42528b6afcf4575a4fde1463d3d1e0
WeiMin-Coding <badmanweimin@gmail.com>
2024-09-04 23:14:41 +0800

    JSON字符串转换Java对象

--- JsonDemo/src/com/java/test/TestJson.java
+++ JsonDemo/src/com/java/test/TestJson.java
@@ -1,6 +1,7 @@
 package com.java.test;
 
 import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import com.java.pojo.Dog;
 import com.java.pojo.Person;
 import org.junit.Test;
@@ -15,5 +16,12 @@ public class TestJson {
         // 将Person对象转换成一个字符串
         String jsonString = JSON.toJSONString(wayneMing);
         System.out.println(jsonString);
+    }
+
+    @Test
+    public void testReadJson() throws Exception{
+        String personString = "{\"age\":555,\"dog\":{\"name\":\"小黄\"},\"name\":\"WayneMing\"}\n";
+        Person wayneMing = JSON.parseObject(personString, Person.class);
+        System.out.println(wayneMing);
     }
 }
