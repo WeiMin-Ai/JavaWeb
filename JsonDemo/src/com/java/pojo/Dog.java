03074872877427c7e64b8566676499ac0143ac11
WeiMin-Coding <badmanweimin@gmail.com>
2024-09-04 23:11:58 +0800

    Java对象转换JSON字符串

---
+++ JsonDemo/src/com/java/pojo/Dog.java
@@ -0,0 +1,35 @@
+package com.java.pojo;
+
+import java.util.Objects;
+
+public class Dog {
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dog dog = (Dog) o;
+        return Objects.equals(name, dog.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public Dog() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ JsonDemo/src/com/java/test/TestJson.java
@@ -0,0 +1,19 @@
+package com.java.test;
+
+import com.alibaba.fastjson2.JSON;
+import com.java.pojo.Dog;
+import com.java.pojo.Person;
+import org.junit.Test;
+
+public class TestJson {
+    @Test
+    public void testReadJson() throws Exception{
+        // 实例化对象
+        Dog dog = new Dog("小黄");
+        Person wayneMing = new Person("WayneMing", 555, dog);
+
+        // 将Person对象转换成一个字符串
+        String jsonString = JSON.toJSONString(wayneMing);
+        System.out.println(jsonString);
+    }
+}
